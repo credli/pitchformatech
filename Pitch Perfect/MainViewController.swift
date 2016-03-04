06ab3878eb1b02d5663071566f6e69473c6eadfc
Nicholas Credli <nicholas.credli@gmail.com>
2016-03-04 21:05:13 +0200

    Add AVFoundation and recording capabilities

--- Pitch Perfect/MainViewController.swift
+++ Pitch Perfect/MainViewController.swift
@@ -7,8 +7,14 @@
 //
 
 import UIKit
+import AVFoundation
 
 class MainViewController: UIViewController {
+    
+    //step 1: include AVFoundation library from the project settings screen
+    
+    //step 2: create an Audio recorder variable which will allow us to record / stop an audio
+    var audioRecoder: AVAudioRecorder!
     
     @IBOutlet var recordButton: UIButton!
     var currentState: MainScreenState = .Stopped
@@ -23,6 +29,24 @@ class MainViewController: UIViewController {
         super.viewDidLoad()
         
         self.navigationItem.title = "Record"
+        
+        //step 3: initialize our audio recorder
+        let audioSession: AVAudioSession = AVAudioSession.sharedInstance()
+        
+        //step 4: check if we have permissions to use the audio
+        if(audioSession.respondsToSelector("requestRecordPermission:")) {
+            audioSession.requestRecordPermission() { (granted: Bool)->Void in
+                if(granted) {
+                    //set category and activate recorder session
+                    try! audioSession.setCategory(AVAudioSessionCategoryPlayAndRecord)
+                    try! audioSession.setActive(true)
+                } else {
+                    //show alert if permission failed
+                    let alertViewController = UIAlertController(title: "Something is wrong", message: "You have not granted us permission to access your microphone", preferredStyle: UIAlertControllerStyle.Alert)
+                    self.presentViewController(alertViewController, animated: true, completion: nil)
+                }
+            }
+        }
     }
 
     override func didReceiveMemoryWarning() {
@@ -46,12 +70,40 @@ class MainViewController: UIViewController {
     
     // this sets to recording
     func setStateRecording() {
+        
+        //step 5: initialize the audio recorder if it wasn't already initialized
+        if self.audioRecoder == nil {
+            let documentsDirectory = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)[0]
+            let fullPath = documentsDirectory.stringByAppendingString("/voiceRecording.caf")
+            print(fullPath)
+            let url = NSURL.fileURLWithPath(fullPath)
+            
+            //create AnyObject of settings
+            let settings: [String : AnyObject] = [
+                AVFormatIDKey:Int(kAudioFormatAppleIMA4), //Int required in Swift2
+                AVSampleRateKey:44100.0,
+                AVNumberOfChannelsKey:2,
+                AVEncoderBitRateKey:12800,
+                AVLinearPCMBitDepthKey:16,
+                AVEncoderAudioQualityKey:AVAudioQuality.Max.rawValue
+            ]
+            
+            try! self.audioRecoder = AVAudioRecorder(URL: url, settings: settings)
+        }
+        
+        //step 6: start recording
+        self.audioRecoder.record()
+        
         self.currentState = .Recording
         self.recordButton?.setTitle("Recording", forState: .Normal)
     }
     
     // this sets to stopped
     func setStateStopped() {
+        
+        //step 7: when we're done, we stop recording
+        self.audioRecoder.stop()
+        
         self.currentState = .Stopped
         self.recordButton?.setTitle("Stopped", forState: .Normal)
         
